a505997d837c4235dc7efd05c64e6a1a9daba90f
iestynlee <19648585+iestynlee@users.noreply.github.com>
2022-03-02 12:47:09 +0000

    Gamemaster added
    Allows the Gamemaster to exist and certain person to see

--- game/forms.py
+++ game/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
-#Models
+#Forms
 class CreateUserForm(UserCreationForm):
 	email = forms.EmailField(required=True) #Telling you that email is required
 
--- game/models.py
+++ game/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Lobby(models.Model):
+	lobby_code = models.CharField(max_length=18, unique=True)
+
+	def players(self):
+		return self.users.all()
+
+#class Tasks(models.Model):
+#	name = models.CharField(max_length=100)
+#	location = models.PointField()
