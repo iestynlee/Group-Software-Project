192e0b933293afed447aaf0f88a68b0150cc1c60
afnang <81368254+afnang@users.noreply.github.com>
2022-03-02 01:32:05 +0000

    implemented the specified features, testing not done yet

--- Game.py
+++ Game.py
@@ -1,6 +1,10 @@
 from enum import Enum
+from operator import le
+from queue import Empty
+from time import sleep
 from typing import List 
 from Player import *
+import random
 
 class State(Enum):
     PENDING = 1
@@ -21,12 +25,44 @@ class Game:
         tasks - 
         gameMaster - the Game Master of the Game
         """
-        pass
+
+        self.noOfPlayers = noOfPlayers
+        self.state = state
+        self.players = []
+        self.tasks = []
+        self.gameMaster = gameMaster
+        
+    @property
+    def noOfPlayers(self):
+        return self.noOfPlayers
+    @noOfPlayers.setter
+    def noOfPlayers(self, newNoOfPlayers):
+        self.noOfPlayers = newNoOfPlayers
+    
+    @property
+    def state(self):
+        return self.state
+    @state.setter
+    def state(self, newState):
+        self.state = newState
+
+    @property
+    def players(self):
+        return self.players
+    @property
+    def tasks(self):
+        return self.tasks
+    @property
+    def gameMaster(self):
+        return self.gameMaster
+    @gameMaster.setter
+    def gameMaster(self, newGameMaster):
+        self.gameMaster = newGameMaster
+        
 
     def startGame(self) -> None:
         """
         This function will be called when the Game Master clicks start game from the game lobby
-
         This functions sets up and starts the game, and then continuously checks for win conditions.
         (This could be done with threading).
         The win conditions are: 
@@ -34,29 +70,47 @@ class Game:
         OR 
         The number of imposters is equal or more to the number of crewmates
         """
-        pass
+        
+        print("Game is starting in 3")
+        sleep(1)
+        print("Game is starting in 2")
+        sleep(1)
+        print("Game is starting in 1")
+        sleep(1)
+        print("Game starting now")
+        self.state = "IN_PROGRESS"
+
+        while self.state == "IN_PROGRESS":
+            if self.players.isAlive <= self.players.isAlive and self.players.isImposter:
+                self.state == "FINISHED"
+
+    
+        
 
     def cancelGame(self) -> None:
         """
         This function is called when the Game Master clicks the cancel game button
-
         This function changes the state of the game to CANCELED and removes all players from the Game
         """
-        pass
+        self.state = "CANCELLED"
+        print("Game master has cancelled the game")
+        self.players.clear()
 
     def finishGame(self) -> None:
         """
         This function is called when a win condition of the game has been met.
-
         This function ends the game, changing the state to finished. 
         A message should be displayed depending on who won.
         """
-        pass
+        if self.players.isAlive <= self.players.isAlive and self.players.isImposter:
+            print("Imposters Win")
+        else:
+            print("Crewmates win")
+        
 
     def addPlayer(self, user) -> Player:
         """
         This function is called when a user joins the game's lobby.
-
         This function creates a player associated with the user and adds it to the game.
         -----
         Paramaters:
@@ -65,12 +119,14 @@ class Game:
         Returns:
         The player added to the game
         """
-        pass
+        self.players.append(user)
+
+        return "Player " + user + " has joined the lobby"
+        
 
     def removePlayer(self, user) -> Player:
         """
         This function is called when a user leaves the game's lobby.
-
         This function removes a player from the game's lobby
         -----
         Paramaters:
@@ -79,7 +135,9 @@ class Game:
         Returns:
         The player removed from the game
         """
-        pass
+        self.players.remove(user)
+        print(user + " has been removed from the game")
+        return user 
 
     def chooseImposter(self) -> List:
         """
@@ -88,13 +146,22 @@ class Game:
         Returns:
         The player(s) chosen to be imposters in a list
         """
-        pass
+    
+        numberOfImposters = input("Choose the desired number of imposters")
+        listOfImposters = random.sample(self.players, numberOfImposters)
+        return listOfImposters
+        
 
     def distributeTasks(self) -> None:
         """
         This function takes all the tasks in the game and distibutes them evenly among 
         the non imposter players.
         """
-        pass
-    
-    
+        noOfIndividualTasks = self.tasks % len(self.players)
+        for i in range(self.players):
+            for x in range(noOfIndividualTasks):
+                self.players.individualTask.append(random.choice(self.tasks))
+
+
+    def addTasks(self, task):
+        self.tasks.append(task)
