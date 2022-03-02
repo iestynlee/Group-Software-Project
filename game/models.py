d4a2a5ee9ab5cc18873831632fb99b231df99c9e
iestynlee <19648585+iestynlee@users.noreply.github.com>
2022-03-02 18:53:07 +0000

    Updating Lobby

--- game/models.py
+++ game/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
+import uuid
 
 class Lobby(models.Model):
-	lobby_code = models.CharField(max_length=18, unique=True)
+	users = models.ManyToManyField(User)
+	lobby_code = models.UUIDField(default=uuid.uuid4, editable=False)
+	#lobby_code = models.CharField(max_length=18, unique=True)
 
 	def players(self):
 		return self.users.all()
 
-#class Tasks(models.Model):
-#	name = models.CharField(max_length=100)
-#	location = models.PointField()
+	def is_occupied(self):
+		return self.users.count() >= 10
