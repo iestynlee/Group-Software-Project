83e3f386a8670d35badc55c33a4ac9d77eaac0a0
Jack Eden <84327867+BlurredButterfly@users.noreply.github.com>
2022-03-23 16:20:21 +0000

    Add files via upload
    added players and tasks to db

--- game/models.py
+++ game/models.py
@@ -1,16 +1,77 @@
+from asyncio.windows_events import NULL
 from django.db import models
 from django.contrib.auth.models import User, Group
 import uuid
 
 class Lobby(models.Model):
-	users = models.ManyToManyField(User)
 	lobby_name = models.CharField(max_length=200, default='Test', unique=True)
+	users = models.ManyToManyField(User)
+	#gameMaster = models.OneToOneField(User, on_delete=models.CASCADE)
+	gameState = models.IntegerField(default=0)
 
 	def __str__(self):
 		return self.lobby_name
-
-	def players(self):
+	def _users(self):
 		return self.users.all()
+	def _players(self):
+		return self.player_set.all()
+	def _is_occupied(self):
+		return self.players.count() >= 10
 
-	def is_occupied(self):
-		return self.users.count() >= 10
+class Player(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	playerName = models.CharField(max_length=200, default="Guest")
+	isImposter = models.BooleanField(default=False)
+	isAlive = models.BooleanField(default=False)
+	gpsLongitude = models.FloatField(default=0)
+	gpsLatitude = models.FloatField(default=0)
+	lobby = models.ForeignKey(Lobby, on_delete=models.CASCADE, default=NULL)
+	color = models.CharField(max_length=10, default="#000000")
+
+
+	def __str__(self):
+		return self.playerName
+	def _user(self):
+		return self.user
+	def _isImposter(self):
+		return self.isImposter
+	def _isAlive(self):
+		return self.isAlive
+	def _gpsLongitude(self):
+		return self.gpsLongitude
+	def _gpsLatitude(self):
+		return self.gpsLatitude
+	def _color(self):
+		return self.color
+
+	def _taskList(self):
+		return self.task_set.all()
+
+
+class Task(models.Model):
+	
+	player = models.ForeignKey(Player, on_delete=models.CASCADE)
+	taskName =  models.CharField(max_length=200, default='Task')
+	gpsLongitude = models.FloatField(default=0)
+	gpsLatitude = models.FloatField(default=0)
+	taskNumber = models.IntegerField(default=0)
+	isDone = models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.taskName
+	def _player(self):
+		return self.player
+	def _gpsLongitude(self):
+		return self.gpsLongitude
+	def _gpsLatitude(self):
+		return self.gpsLatitude
+	def _taskNumber(self):
+		return self.taskNumber
+	def _isDone(self):
+		return self.isDone
+	
+
+
+
+
+
