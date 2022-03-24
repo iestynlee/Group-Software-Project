6407754489a27ba961ed708ad9ab60d44365823d
iestynlee <19648585+iestynlee@users.noreply.github.com>
2022-03-24 05:09:28 +0000

    Add files via upload

--- game/utils.py
+++ game/utils.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import Group
 
+#This function is just used to check who is gamemaster or not
 def is_gamemaster(user):
 	return user.groups.filter(name='Gamemaster').exists()
